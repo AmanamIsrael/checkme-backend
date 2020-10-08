59802d6ca89a912cd24aedf954a536397e453b3a
Amanam Israel <amanam42@hotmail.com>
2020-10-09 00:38:13 +0100

    installed cors & initialized

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
 
-//connect to database
+app.get('/', (req, res) => {
+    res.send('this app works');
+})
+
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.qoybl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
