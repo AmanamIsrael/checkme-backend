d53dc5c7222a3454deb5769ccab42b27c12f8fe6
Amanam Israel <amanam42@hotmail.com>
2020-10-09 15:03:36 +0100

    changed all res.send to res.json

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('this app works');
+    res.json('this app works');
 })
 
 const authRoute = require('./routes/auth');
